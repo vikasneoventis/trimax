0d8a6890d1b864761be9c4a984a9eb24c94d7908
ashok <sanjay@evincedev.com>
2018-05-03 14:53:17 +0530

    trimax theme

---
+++ app/design/frontend/Trimax/default/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Evince Team
+ * @copyright Copyright (c) 2018 Evince (http://evincemage.com/)
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Trimax/default',
+    __DIR__
+);
