387a7c9dec87ae957f2940fbe57033c8efb44b2a
ashok <sanjay@evincedev.com>
2018-05-23 15:17:58 +0530

    top menu module

--- phpinfo.php
+++ phpinfo.php
@@ -1,3 +1,35 @@
 <?php
 
-phpinfo() ;
+//phpinfo() ;
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "trimax";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$customerGroup = array(3,4,5,6,7,8,10,11,12,13,14,15,16);
+$sql = '';
+for($i=2; $i < 57; $i++)
+{
+    foreach ($customerGroup as $_group) {
+        $sql .= "INSERT INTO `mageworx_downloads_attachment_customer_group`(`attachment_id`, `customer_group_id`)
+VALUES ($i,$_group);";
+    }
+}
+
+echo $sql;die;
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ test.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @author Evince Team
+ * @copyright Copyright (c) 2018 Evince (http://evincemage.com/)
+ */
+
+error_reporting(1);
+
+
+use \Magento\Framework\App\Bootstrap;
+
+require __DIR__ . '/app/bootstrap.php';
+$bootstrap = Bootstrap::create(BP, $_SERVER);
+$objectManager = $bootstrap->getObjectManager();
+$instance = \Magento\Framework\App\ObjectManager::getInstance();
+$state = $objectManager->get('\Magento\Framework\App\State');
+$state->setAreaCode(\Magento\Framework\App\Area::AREA_ADMINHTML);
+
+$connection = $objectManager->get('\Magento\Framework\App\ResourceConnection')->getConnection();
+$storeTable = $connection->getTableName('store');
+$query = "SELECT `store_id`,`code` FROM $storeTable";
+$store = $connection->fetchAll($query);
+
+$storeData = array();
+foreach ($store as $_store){
+    $storeData[$_store['code']] = $_store['store_id'];
+}
+
+$stockData = array();
+$file_handle = fopen('Stock-update.csv', 'r');
+while (!feof($file_handle) ) {
+    $stockData[] = fgetcsv($file_handle, 1024);
+}
+fclose($file_handle);
+
+$respone = [];
+foreach ($stockData as $key=> $_data) {
+    $sku = trim($_data['0']);
+    $storeCode = trim($_data['1']);
+    $qty = (int)trim($_data['2']);
+
+    if($key == 0 || $sku == null || $storeCode == null || $qty == null)continue;
+
+    $product = $objectManager->create('\Magento\Catalog\Model\ProductRepository')->get($sku);
+
+    $productId = $product->getId();
+
+    if ($productId) {
+
+        $stockId = $storeData[$storeCode];
+        $stockTable = $connection->getTableName('warehouseinventory_stock_item');
+        $query = "SELECT `item_id` FROM $stockTable WHERE `product_id`= $productId AND `stock_id` = $stockId";
+        $itemData = $connection->fetchRow($query);
+
+        $itemModel = $objectManager->get('\Eadesigndev\Warehouses\Model\StockItemsRepository');
+        $items = $objectManager->get('\Eadesigndev\Warehouses\Model\StockItemsRepository')->getById($itemData['item_id']);
+
+        try {
+
+            $items->setData(array_merge($items->getData(), array('qty'=>$qty)));
+            $itemModel->save($items);
+        } catch (\Exception $e) {
+            $respone[] = $e->getMessage();
+        }
+
+    } else {
+        $respone[]=  $sku." sku not found";
+    }
+}
+
+echo '<pre>'; print_r($respone); die;
