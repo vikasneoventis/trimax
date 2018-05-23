17683c56a9090048535d01a48f933d25533462e2
ashok <sanjay@evincedev.com>
2018-05-23 15:25:58 +0530

    top menu hide

--- app/code/Evincemage/Topmenu/Block/Downloadcategory.php
+++ app/code/Evincemage/Topmenu/Block/Downloadcategory.php
@@ -6,13 +6,23 @@
 
 namespace Evincemage\Topmenu\Block;
 
+use Magento\Customer\Model\Context;
 
 class Downloadcategory extends \Magento\Framework\View\Element\Template
 {
+    protected $httpContext;
+
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\App\Http\Context $httpContext,
         array $data = []
     ) {
         parent::__construct($context,$data);
+        $this->httpContext = $httpContext;
+    }
+
+    public function isLoggedIn()
+    {
+        return $this->httpContext->getValue(Context::CONTEXT_AUTH);
     }
 }
