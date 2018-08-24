ffe83364b58c961043afa442e7aecd2535d9587e
ashok <sanjay@evincedev.com>
2018-08-24 11:32:23 +0530

    update source

--- lib/web/ext/AbandonedCartAlertsPro/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Aitoc_AbandonedCartAlertsPro',
-    __DIR__
-);
--- lib/web/ext/CheckoutFieldsManager/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Aitoc_CheckoutFieldsManager',
-    __DIR__
-);
